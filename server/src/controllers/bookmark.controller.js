4f91ba09d5f89b2b6ac2f16965d5301cbc64b2fb
Pariharx7 <parihar.peace@gmail.com>
2025-10-02 13:52:45 +0530

    Updated server directory

---
+++ server/src/controllers/bookmark.controller.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const getAllBookmarks = asyncHandler(async (req, res) => {
+
+})
+
+const addABookmark = asyncHandler(async (req, res) => {
+    
+})
+
+const deleteABookmark = asyncHandler(async (req, res) => {
+    
+})
+
+export {
+    getAllBookmarks,
+    addABookmark,
+    deleteABookmark
+}
