e774cbb81bc01de6b1c8be4a2cc2a3ba92ca24a2
Pariharx7 <parihar.peace@gmail.com>
2026-05-16 18:15:01 +0530

    added a common file for importing all the files via a single file

--- server/src/api/common/utils/index.js
+++ server/src/api/common/utils/index.js
@@ -1,5 +1,7 @@
-import { ApiError } from "./ApiError";
-import { asyncHandler } from "./asyncHandler";
-import { ApiResponse } from "./ApiResponse";
+import { ApiError } from "./ApiError.js";
+import { asyncHandler } from "./asyncHandler.js";
+import { ApiResponse } from "./ApiResponse.js";
+import { getAvatarName } from "./avatarHelper.js";
+import { paginateQuery } from "./pagination.js";
 
-export { ApiError, asyncHandler, ApiResponse };
+export { ApiError, asyncHandler, ApiResponse, getAvatarName, paginateQuery };
