1a36313334d078263133acccd66687f67e86c0b3
Pariharx7 <parihar.peace@gmail.com>
2026-05-17 19:42:03 +0530

    fixed import issues

--- server/src/config/winston.logger.js
+++ server/src/config/winston.logger.js
@@ -1,33 +1,33 @@
-import winston from winston;
-import 'winston-mongodb';
+import winston from "winston";
+import "winston-mongodb";
 
-const {createLogger, transports, format} = winston;
-const {combine, cli, timestamp, json, metadata} = format;
+const { createLogger, transports, format } = winston;
+const { combine, cli, timestamp, json, metadata } = format;
 
-const envMode = process.env.NODE_ENV || 'development';
+const envMode = process.env.NODE_ENV || "development";
 
 winston.addColors({
-    error: 'red',
-    warn: 'yellow',
-    info: 'blue',
-    verbose: 'purple',
-    debug: 'white',
+  error: "red",
+  warn: "yellow",
+  info: "blue",
+  verbose: "purple",
+  debug: "white",
 });
 
 const logger = createLogger({
-    transports: [
-        new transports.Console({
-            level: envMode === 'development' ? 'debug' : 'info',
-            format: cli(),
-        }),
-        new transports.MongoDB({
-            level: 'error',
-            format: combine(timestamp(), json(), metadata()),
-            options: {useUnifiedTopology: true},
-            db: process.env.DB_URL,
-            collection: 'logs-winston',
-        }),
-    ],
+  transports: [
+    new transports.Console({
+      level: envMode === "development" ? "debug" : "info",
+      format: cli(),
+    }),
+    new transports.MongoDB({
+      level: "error",
+      format: combine(timestamp(), json(), metadata()),
+      options: { useUnifiedTopology: true },
+      db: process.env.DB_URL,
+      collection: "logs-winston",
+    }),
+  ],
 });
 
 export default logger;
