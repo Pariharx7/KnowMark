3ab262a31c4c99cfa33b3ab4580ebf9597e57f57
Pariharx7 <parihar.peace@gmail.com>
2026-05-18 19:14:15 +0530

    user schema modification

--- server/src/api/common/models/user.model.js
+++ server/src/api/common/models/user.model.js
@@ -62,28 +62,28 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 };
 
 userSchema.methods.generateAccessToken = function () {
-  return jwt.sign(
+  const accessToken = jwt.sign(
     {
       _id: this._id,
       email: this.email,
     },
     process.env.ACCESS_TOKEN_SECRET,
-    {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-    }
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
   );
+
+  return accessToken;
 };
 
-userSchema.methods.generateRefereshToken = function () {
-  return jwt.sign(
+userSchema.methods.generateRefreshToken = function () {
+  const refreshToken = jwt.sign(
     {
       _id: this._id,
     },
     process.env.REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-    }
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
   );
+
+  return refreshToken;
 };
 
 export const User = mongoose.model("User", userSchema);
