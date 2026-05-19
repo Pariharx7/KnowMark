6971a13322157daa39a7e5a24dd4a5691b00a8aa
Pariharx7 <parihar.peace@gmail.com>
2026-05-19 18:32:36 +0530

    route to check the app health/status

--- server/src/api/common/routes/healthcheck.route.js
+++ server/src/api/common/routes/healthcheck.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { serverHealthCheck } from "../controllers/index";
+import { serverHealthCheck } from "../controllers/index.js";
 
 const router = Router();
 
