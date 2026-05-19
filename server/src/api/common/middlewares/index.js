bd06639d15ce79adb5dda7d743178c5102d7dd4d
Pariharx7 <parihar.peace@gmail.com>
2026-05-19 18:32:11 +0530

    index file for middlewares folder

--- server/src/api/common/middlewares/index.js
+++ server/src/api/common/middlewares/index.js
@@ -1,5 +1,5 @@
-import { errorHandler } from "./error.middleware";
-import { authorization } from "./auth.middleware";
-import { passport } from "./passport.middleware";
+import { errorHandler } from "./error.middleware.js";
+import { authorization } from "./auth.middleware.js";
+import { passport } from "./passport.middleware.js";
 
 export { errorHandler, authorization, passport };
