02f106807bf25a4b7583d5d0f36ad13d82074a1c
Pariharx7 <parihar.peace@gmail.com>
2026-05-19 18:31:03 +0530

    middleware for error handling

--- server/src/api/common/middlewares/error.middleware.js
+++ server/src/api/common/middlewares/error.middleware.js
@@ -3,10 +3,21 @@ import mongoose from "mongoose";
 import { ApiError } from "../utils/index.js";
 import { logger } from "../../../config/index.js";
 
+/**
+ *
+ * @param {Error | ApiError} err
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ *
+ *
+ * @description
+ *
+ */
 const errorHandler = (err, req, res, next) => {
   let error = err;
 
-  if ((!error) instanceof ApiError) {
+  if (!(error instanceof ApiError)) {
     const statusCode =
       error.statusCode || error instanceof mongoose.Error ? 400 : 500;
 
@@ -14,13 +25,16 @@ const errorHandler = (err, req, res, next) => {
 
     error = new ApiError(statusCode, message, error?.errors || [], err.stack);
   }
+
   const response = {
     ...error,
     message: error.message,
     ...(process.env.NODE_ENV === "development" ? { stack: error.stack } : {}),
   };
+
   logger.error(`${error.message}`, { metadata: error });
 
+  // Send error response
   return res.status(error.statusCode).json(response);
 };
 
