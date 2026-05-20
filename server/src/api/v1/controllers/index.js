d617cb220edb71f1e309e6e464fd058843ebbf34
Pariharx7 <parihar.peace@gmail.com>
2026-05-20 14:20:27 +0530

    added index file for controllers

---
+++ server/src/api/v1/controllers/index.js
@@ -0,0 +1,5 @@
+import { authController } from "./auth.controller.js";
+import { userController } from "./user.controller.js";
+import { bookmarkController } from "./bookmark.controller.js";
+
+export { authController, userController, bookmarkController };
