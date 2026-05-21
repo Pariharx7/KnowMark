d71967d8450f24970039b1273eae8ef270992ecd
Pariharx7 <parihar.peace@gmail.com>
2026-05-21 18:00:31 +0530

    added index file for routes

---
+++ server/src/api/v1/routes/index.js
@@ -0,0 +1,5 @@
+import authRouter from "./auth.route.js";
+import userRouter from "./user.route.js";
+import bookmarkRouter from "./bookmark.route.js";
+
+export { authRouter, userRouter, bookmarkRouter };
