72f1e4b44a928e837503b97107e038fb7959ccaf
Pariharx7 <parihar.peace@gmail.com>
2026-05-22 19:43:13 +0530

    added index file for services/

---
+++ server/src/api/v1/services/index.js
@@ -0,0 +1,5 @@
+import { authService } from "./auth.service.js";
+import { userService } from "./user.service.js";
+import { bookmarkService } from "./bookmark.service.js";
+
+export { authService, userService, bookmarkService };
