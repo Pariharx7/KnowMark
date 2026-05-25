485fdab436b47e1e9c16c15b5b183135c79d88ab
Pariharx7 <parihar.peace@gmail.com>
2026-05-25 19:50:49 +0530

    added index file for features/authentication/components/

---
+++ client/src/features/authentication/components/index.js
@@ -0,0 +1,6 @@
+import GoogleSignin from "./GoogleSignin";
+import GuestSignin from "./GuestSignin";
+import SignupForm from "./SignupForm";
+import SigninForm from "./SigninForm";
+
+export { GoogleSignin, SignupForm, SigninForm, GuestSignin };
