d18cb964540a808489b73f15e197c18fcd282ff4
Pariharx7 <parihar.peace@gmail.com>
2026-05-26 19:01:14 +0530

    added index file for components/ui/

---
+++ client/src/components/ui/index.js
@@ -0,0 +1,6 @@
+import InputBox from "./InputBox";
+import PasswordBox from "./PasswordBox";
+import Button from "./Button";
+import SearchBox from "./SearchBox";
+
+export { InputBox, PasswordBox, Button, SearchBox };
