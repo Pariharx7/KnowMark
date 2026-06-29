2f14101a79eeb4677d2d63b45c28ddd040486cd0
Pariharx7 <parihar.peace@gmail.com>
2026-06-29 16:51:26 +0530

    updated bookmark validator

--- server/src/api/v1/validators/bookmark.validator.js
+++ server/src/api/v1/validators/bookmark.validator.js
@@ -3,20 +3,29 @@ import z, { array } from "zod";
 const bookmarkCreationValidator = (bookmark) => {
   const schema = z.object({
     url: z.string().url({ message: "Please provide a valid URL" }).nonempty(),
-    title: z.string().min(3).max(255).nonempty().trim(),
-    notes: z.string().optional().nullable().max(1000),
-    tags: z.array(z.string().trim().length(30).toLowerCase()).optional(),
-    category: z.string().optional().trim().max(50),
+    title: z.string().min(3).max(255).trim().nonempty(),
+    notes: z.string().max(999).optional().nullable(),
+    tags: z.array(z.string().trim().min(2).max(19).toLowerCase()).optional(),
+    category: z.string().trim().max(50).optional(),
   });
   return schema.safeParse(bookmark);
 };
 
 const bookmarkUpdateValidator = (bookmark) => {
   const schema = z.object({
-    title: z.string().min(3).max(255).nonempty().trim().optional(),
-    notes: z.string().optional().nullable().max(1000),
-    tags: z.array(z.string().trim().length(30).toLowerCase()).optional(),
-    category: z.string().optional().trim().max(50),
+    title: z.string().min(3).max(255).trim().nonempty().optional(),
+    notes: z.string().max(1000).optional().nullable(),
+    tags: z
+      .array(
+        z
+          .string()
+          .max(9, "Each tag must be 9 characters or less")
+          .trim()
+          .toLowerCase()
+      )
+      .max(4, "You can have a maximum of 4 tags")
+      .optional(),
+    category: z.string().trim().max(50).optional(),
   });
   return schema.safeParse(bookmark);
 };
