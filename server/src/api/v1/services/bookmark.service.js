d3c945731c52aa40a1dbc20c02fee4f339c1ff0c
Pariharx7 <parihar.peace@gmail.com>
2026-06-29 16:51:59 +0530

    updated bookmark service

--- server/src/api/v1/services/bookmark.service.js
+++ server/src/api/v1/services/bookmark.service.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { Bookmark } from "../../common/models/index.js";
+import { User } from "../../common/models/index.js";
 import { paginateQuery, ApiError } from "../../common/utils/index.js";
 
 const getAllBookmarks = async (userId, page, limit) => {
@@ -8,22 +9,60 @@ const getAllBookmarks = async (userId, page, limit) => {
   })
     .populate("userId", "name")
     .select("-__v")
-    .sort({ createdAt: -1 });
-
+    .sort({ createdAt: -1 })
+    .lean();
   const result = await paginateQuery(bookmarksQuery, page, limit);
 
   const bookmarksList = result.data.map((bookmark) => {
     const utcDate = new Date(bookmark.createdAt);
-
     return {
       user: bookmark.userId?.name || "User",
-      name: bookmark.name,
+      id: bookmark._id,
+      name: bookmark.title,
+      url: bookmark.url,
+      notes: bookmark.notes,
       date: utcDate.toLocaleDateString("en-GB"),
       time: utcDate.toLocaleTimeString("en-US", { timeZone: "Asia/Kolkata" }),
     };
   });
 
   return { bookmarks: bookmarksList, pagination: result.pagination };
+};
+
+const getBookmarkById = async (userId, bookmarkId) => {
+  if (!mongoose.Types.ObjectId.isValid(bookmarkId)) {
+    throw new ApiError(400, "Invalid Bookmark Id");
+  }
+
+  const [bookmarkById, user] = await Promise.all([
+    Bookmark.findOne({ _id: bookmarkId, userId })
+      .select("-__v")
+      .sort({ createdAt: -1 })
+      .lean(),
+    User.findById(userId).select("starredBookmarks name").lean(),
+  ]);
+
+  const starredBookmarksArray = user?.starredBookmarks.map(
+    (id) => id.toString() || []
+  );
+  // console.log("50 User ", user.name);
+
+  const isStarred = starredBookmarksArray.includes(bookmarkId);
+  const utcDate = new Date(bookmarkById.createdAt);
+  const bookmark = {
+    user: user.name || "You",
+    id: bookmarkById._id,
+    title: bookmarkById.title,
+    url: bookmarkById.url,
+    notes: bookmarkById.notes,
+    tags: bookmarkById.tags,
+    category: bookmarkById.category,
+    date: utcDate.toLocaleDateString("en-GB"),
+    time: utcDate.toLocaleTimeString("en-US", { timeZone: "Asia/Kolkata" }),
+    isStarred: starredBookmarksArray.includes(bookmarkId),
+  };
+
+  return { bookmark: bookmark, isStarred: isStarred };
 };
 
 const addBookmark = async (userId, url, title, notes, tags, category) => {
@@ -60,25 +99,117 @@ const deleteBookmark = async (userId, bookmarkId) => {
 const updateBookmark = async (userId, bookmarkId, bookmarkData) => {
   const { url, title, notes, tags, category } = bookmarkData;
 
-  const bookmark = await Bookmark.findById({ _id: bookmarkId, userId });
+  const bookmark = await Bookmark.findOne({ _id: bookmarkId, userId });
+  if (!bookmark) {
+    throw new ApiError(404, "Bookmark not found or not authorized to update");
+  }
 
-  if (url) bookmark.url = url;
-  if (title) bookmark.url = title;
-  if (notes) bookmark.url = notes;
-  if (tags) bookmark.url = tags;
-  if (category) bookmark.url = category;
+  if (title) bookmark.title = title;
+  if (notes) bookmark.notes = notes;
+  if (tags) bookmark.tags = tags;
+  if (category) bookmark.category = category;
 
   await bookmark.save();
 
-  return bookmark.findById({ _id: bookmarkId, userId });
+  return bookmark;
 };
-//requires pagination
-const searchBookmark = async () => {};
+
+const searchBookmark = async (query, page, limit, userId) => {
+  const escapedQuery = query.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  const regex = new RegExp(escapedQuery, "i");
+
+  const bookmarksQuery = Bookmark.find({
+    userId,
+    $or: [
+      { title: regex },
+      { notes: regex },
+      { category: regex },
+      { url: regex },
+      { tags: regex },
+    ],
+  })
+    .select("-__v")
+    .sort({ createdAt: -1 });
+
+  const result = await paginateQuery(bookmarksQuery, page, limit);
+
+  const bookmarksList = result.data.map((bookmark) => ({
+    id: bookmark.id,
+    title: bookmark.title,
+    url: bookmark.url,
+    tags: bookmark.tags,
+    category: bookmark.category,
+    notes: bookmark.notes,
+  }));
+
+  return { bookmarks: bookmarksList, pagination: result.pagination };
+};
+
+const getStarredBookmarks = async (userId, page, limit) => {
+  const user = await User.findById(userId).select("starredBookmarks");
+
+  const starredBookmarkIds = user.starredBookmarks.map(
+    (bookmarkId) => bookmarkId
+  );
+
+  const starredBookmarksQuery = Bookmark.find({
+    _id: { $in: starredBookmarkIds },
+    userId,
+  })
+    .populate("userId", "name")
+    .select("-__v")
+    .sort({ createdAt: -1 });
+
+  const result = await paginateQuery(starredBookmarksQuery, page, limit);
+
+  const starredBookmarksList = result.data.map((bookmark) => {
+    const utcDate = new Date(bookmark.createdAt);
+
+    return {
+      user: bookmark.userId?.name || "User",
+      id: bookmark.id,
+      name: bookmark.title,
+      url: bookmark.url,
+      tags: bookmark.tags,
+      category: bookmark.category,
+      notes: bookmark.notes,
+      date: utcDate.toLocaleDateString("en-GB"),
+      time: utcDate.toLocaleTimeString("en-US", { timeZone: "Asia/Kolkata" }),
+    };
+  });
+
+  return {
+    bookmarks: starredBookmarksList,
+    pagination: result.pagination,
+  };
+};
+
+const starBookmark = async (userId, bookmarkId) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    { $addToSet: { starredBookmarks: bookmarkId } },
+    { new: true }
+  );
+  return { user: updatedUser };
+};
+
+const unstarBookmark = async (userId, bookmarkId) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    { $pull: { starredBookmarks: bookmarkId } },
+    { new: true }
+  );
+  return { user: updatedUser };
+};
 
 export const bookmarkService = {
   getBookmarks: getAllBookmarks,
+  getBookmark: getBookmarkById,
   addBookmark,
   deleteBookmark,
   updateBookmark,
   searchBookmark,
+  getStarredBookmarks,
+  starBookmark,
+  unstarBookmark,
 };
