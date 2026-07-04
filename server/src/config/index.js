e76157d9278eb59f516a68152401b97229640d5d
Pariharx7 <parihar.peace@gmail.com>
2026-07-04 12:58:44 +0530

    added export for cloudinary  service

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,5 +1,6 @@
 import connectDB from "./db.js";
 import logger from "./winston.logger.js";
 import morganMiddleware from "./morgan.logger.js";
+import cloudinary from "./cloudinary.service.js";
 
-export { connectDB, logger, morganMiddleware };
+export { connectDB, logger, morganMiddleware, cloudinary };
