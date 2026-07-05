baa6901bbceca866217f002b3bcac06d90b1e3f9
Pariharx7 <parihar.peace@gmail.com>
2026-07-05 16:26:23 +0530

    removed unused imports

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,6 +1,5 @@
 import connectDB from "./db.js";
 import logger from "./winston.logger.js";
 import morganMiddleware from "./morgan.logger.js";
-import cloudinary from "./cloudinary.service.js";
 
-export { connectDB, logger, morganMiddleware, cloudinary };
+export { connectDB, logger, morganMiddleware };
