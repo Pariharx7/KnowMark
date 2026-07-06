088de86453f4108279d97bb46b688884a79b7e0b
Pariharx7 <parihar.peace@gmail.com>
2026-07-06 15:45:30 +0530

    updated the limit size of express json middleware

--- server/src/app.js
+++ server/src/app.js
@@ -47,7 +47,7 @@ app.use(compression());
 app.use(hpp());
 app.use(requestIp.mw());
 app.use("/api/", limiter);
-app.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
