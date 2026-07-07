ac17ac7f16050fe19086ac1e9b952b9b34dfd4c6
Pariharx7 <parihar.peace@gmail.com>
2026-07-07 15:32:11 +0530

    added constants file for qeact query keys

---
+++ client/src/config/constants.js
@@ -0,0 +1,7 @@
+export const BOOKMARK_QUERY_KEYS = {
+  getAllBookmarks: "allBookmarks",
+  getAllStarredBookmarks: "allStarredBookmarks",
+  //cerate one for create bookmark
+  getAllStarredBookmarks: "allStarredBookmarks",
+  getBookmark: "getBookmark",
+};
