7fb1ce444545c75ee32dad39e0591ba414dc7e29
Pariharx7 <parihar.peace@gmail.com>
2026-07-07 15:33:19 +0530

    added config file for nav links

---
+++ client/src/config/navigation.js
@@ -0,0 +1,15 @@
+import {
+  HiOutlineSquares2X2, // Dashboard
+  HiOutlineMagnifyingGlass, // Search
+  HiOutlineStar, // Starred
+  HiOutlineUser, // User Profile
+  HiOutlineCog6Tooth, // User Settings
+} from "react-icons/hi2";
+
+export const navLinks = [
+  { title: "Dashboard", href: "/dashboard", icon: HiOutlineSquares2X2 },
+  { title: "Search", href: "/search", icon: HiOutlineMagnifyingGlass },
+  { title: "Starred", href: "/starred", icon: HiOutlineStar },
+  { title: "User Profile", href: "/profile", icon: HiOutlineUser },
+  { title: "User Settings", href: "/settings", icon: HiOutlineCog6Tooth },
+];
