fbea99a055c0d9528e01d409d37eb763c91ef5dd
Pariharx7 <parihar.peace@gmail.com>
2026-07-16 13:11:00 +0530

    added TagInput component

---
+++ client/src/components/ui/TagInput.jsx
@@ -0,0 +1,45 @@
+const TagInput = ({ props, tags, setTags, error }) => {
+    const { onChange: registerOnChange, ...restProps } = props || {};
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+
+        if (!value.trim()) {
+            setTags([]);
+            if (registerOnChange) registerOnChange(e);
+            return;
+        }
+
+        const rawArray = value.split(',').map(item => item.trim()).filter(Boolean);
+
+        const limitedArray = rawArray.slice(0, 4);
+
+        setTags(limitedArray);
+        if (registerOnChange) registerOnChange(e);
+    };
+
+    return (
+        <div className="w-full">
+            <label className="block text-sm font-semibold text-gray-700 mb-1">Tags(Max 4, Optional)</label>
+            <input
+                {...restProps}
+                type="text"
+                placeholder="e.g tech, react, tailwindcss"
+                // value={tags.join(', ')}
+                onChange={handleChange}
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm"
+            />
+            <p className="mt-1 text-xs text-gray-500">Seperated by commas</p>
+            {
+                error && (
+                    <p className="pl-1 pt-1 text-xs font-normal leading-tight text-red-400">
+                        {error}
+                    </p>
+                )
+            }
+        </div>
+    )
+
+}
+
+export default TagInput;
