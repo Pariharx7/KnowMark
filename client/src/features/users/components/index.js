b92e2d6227484176eac6c29e88406febef8bc899
Pariharx7 <parihar.peace@gmail.com>
2026-07-29 12:44:35 +0530

    index file for features/users/components

---
+++ client/src/features/users/components/index.js
@@ -0,0 +1,11 @@
+import UpdatePasswordForm from "./UpdatePasswordForm";
+import UpdateProfileForm from "./UpdateProfileForm";
+import UserDetails from "./UserDetails";
+import UpdateProfilePicture from "./UpdateProfilePicture";
+
+export {
+  UpdatePasswordForm,
+  UpdateProfileForm,
+  UpdateProfilePicture,
+  UserDetails,
+};
