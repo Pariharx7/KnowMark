2134df14da93af59a53dd8718b1d357d160a6fc2
Pariharx7 <parihar.peace@gmail.com>
2026-08-01 12:02:48 +0530

    added the defaultValue attribute instead of value attribute

--- client/src/components/ui/TagInput.jsx
+++ client/src/components/ui/TagInput.jsx
@@ -25,8 +25,8 @@ const TagInput = ({ props, tags, setTags, error }) => {
                 {...restProps}
                 type="text"
                 placeholder="e.g tech, react, tailwindcss"
-                value={(tags || []).join(', ')}
                 onChange={handleChange}
+                defaultValue={(tags || []).join(', ')}
                 className="w-full px-3 h-20 py-1 text-xl border border-gray-300 rounded-lg shadow-sm"
             />
             <p className="mt-1 text-xs text-gray-500">Seperated by commas</p>
