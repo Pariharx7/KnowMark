81409a6e5bb82dc6f496e466f765ca325507b3dc
Pariharx7 <parihar.peace@gmail.com>
2026-08-04 12:03:05 +0530

    index file for /features/users/

---
+++ client/src/features/users/index.js
@@ -0,0 +1,17 @@
+import UserService from "./services/user.service";
+import useCurrentUser from "./hooks/useCurrentUser";
+import {
+  UpdateProfileForm,
+  UserDetails,
+  UpdateProfilePicture,
+} from "./components";
+import { UpdatePassword } from "./pages";
+
+export {
+  UserService,
+  useCurrentUser,
+  UpdatePassword,
+  UpdateProfileForm,
+  UpdateProfilePicture,
+  UserDetails,
+};
