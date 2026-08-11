a0bea2bc38809fd76ccb0cea62e0a92f86339779
Pariharx7 <parihar.peace@gmail.com>
2026-08-11 10:53:45 +0530

    added styles for dark mode

--- client/src/components/ui/PasswordBox.jsx
+++ client/src/components/ui/PasswordBox.jsx
@@ -19,7 +19,7 @@ const PasswordBox = ({ props, label, type, placeholder, error, id }) => {
                         className="ring-offset-backgroundoutline-none flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 dark:border-dark-800 dark:placeholder:text-[#9CA3AF]"
                     />
                     <button
-                        className="h-10 p-2 pr-2 dark:invert dark:filter cursor-pointer" onClick={() => setShowPassword(!showPassword)}
+                        className="h-10 p-2 pr-2 dark:invert dark:filter dark:text-black cursor-pointer" onClick={() => setShowPassword(!showPassword)}
                     >
                         {showPassword ? <LuEyeOff /> : <LuEye />}
 
