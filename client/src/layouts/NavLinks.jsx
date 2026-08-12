9d35ab34373cf29b3bdf25f83fb1178ca7a0c0cc
Pariharx7 <parihar.peace@gmail.com>
2026-08-12 11:40:51 +0530

    updated styling according to dark mode

--- client/src/layouts/NavLinks.jsx
+++ client/src/layouts/NavLinks.jsx
@@ -4,7 +4,7 @@ const NavLinks = ({ label, Icon, link, onClick }) => {
     const active =
         'flex w-full items-center space-x-3 rounded-md border-transparent bg-primary/5 dark:bg-primary-400/20 px-4 py-2.5 text-sm font-medium text-primary-700 dark:text-white ring-transparent transition-colors duration-100';
     const inactive =
-        'flex w-full items-center space-x-3 rounded-md border-transparent px-4 py-2.5 text-sm dark:text-gray-300 ring-transparent transition-colors duration-100 hover:bg-gray-50 dark:hover:bg-dark-800/40 dark:hover:text-white';
+        'flex w-full items-center space-x-3 rounded-md border-transparent px-4 py-2.5 text-sm dark:text-gray-300 ring-transparent transition-colors duration-100 hover:bg-gray-50 dark:hover:bg-dark-800/40 dark:hover:text-black';
 
     return (
         <NavLink
@@ -14,7 +14,7 @@ const NavLinks = ({ label, Icon, link, onClick }) => {
         >
             <
                 Icon
-                className="h-5 flex-none transition-colors dark:invert dark:filter"
+                className="h-5 flex-none transition-colors dark:filter"
             />
             {label && <span className="truncate hover:text-clip">{label}</span>}
         </NavLink>
