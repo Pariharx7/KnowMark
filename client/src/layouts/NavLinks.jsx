c6ad261cadca46df286a6467bbe756a25b910dea
Pariharx7 <parihar.peace@gmail.com>
2026-08-13 11:04:44 +0530

    updated styling according to dark mode

--- client/src/layouts/NavLinks.jsx
+++ client/src/layouts/NavLinks.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 const NavLinks = ({ label, Icon, link, onClick }) => {
     const active =
-        'flex w-full items-center space-x-3 rounded-md border-transparent bg-primary/5 dark:bg-primary-400/20 px-4 py-2.5 text-sm font-medium text-primary-700 dark:text-white ring-transparent transition-colors duration-100';
+        'flex w-full items-center space-x-3 rounded-md border-transparent bg-primary/5 dark:bg-primary-400/20 px-4 py-2.5 text-sm font-medium text-primary-500 dark:text-white ring-transparent transition-colors duration-100';
     const inactive =
         'flex w-full items-center space-x-3 rounded-md border-transparent px-4 py-2.5 text-sm dark:text-gray-300 ring-transparent transition-colors duration-100 hover:bg-gray-50 dark:hover:bg-dark-800/40 dark:hover:text-black';
 
