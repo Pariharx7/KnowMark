b925aab654976564725c098997878cf9bf9d9bc1
Pariharx7 <parihar.peace@gmail.com>
2026-08-18 10:58:29 +0530

    updated styling according to dark theme

--- client/src/features/bookmark/components/BookmarkItem.jsx
+++ client/src/features/bookmark/components/BookmarkItem.jsx
@@ -87,12 +87,12 @@ const FullBookmark = ({ id, title, url, notes, tags, category, date, time, user,
             <div className="prose prose-slate max-w-none py-8 text-lg leading-relaxed text-slate-700 flex flex-col">
                 <span className="text-xl font-semibold mr-3 text-accent-foreground">Notes: </span>
                 {notesHtml ? (
-                    <div className="mt-2 ml-3" dangerouslySetInnerHTML={{ __html: notesHtml }} />
+                    <div className="mt-2 ml-3 text-secondary" dangerouslySetInnerHTML={{ __html: notesHtml }} />
                 ) : (
                     <span className="italic">No notes available yet. You can edit and add notes</span>
                 )}
-                <p className="flex flex-row gap-2"><span className="text-xl font-semibold mr-3 text-accent-foreground">Tags:  </span>{tags || <span className="italic">No tags available yet. You can edit and add notes</span>}</p>
-                <p><span className="text-xl font-semibold mr-3 text-accent-foreground">Category: </span>{category || <span className="italic">No category available yet. You can edit and add notes</span>}</p>
+                <p className="flex flex-row gap-2 text-secondary"><span className="text-xl font-semibold mr-3 text-accent-foreground">Tags:  </span>{tags || <span className="italic">No tags available yet. You can edit and add tags</span>}</p>
+                <p><span className="text-xl font-semibold mr-3 text-accent-foreground">Category: </span>{category || <span className="italic">No category available yet. You can edit and add category</span>}</p>
             </div>
             <div className="prose prose-slate max-w-none py-8 text-lg leading-relaxed text-slate-700 flex flex-col">
                 <p><span className="text-xl font-semibold mr-3 text-accent-foreground">Created on: </span>{date}<span className="text-xl font-semibold mx-2">at: </span>{time} <span className="text-xl font-semibold mx-2">by: </span>{user}</p>
