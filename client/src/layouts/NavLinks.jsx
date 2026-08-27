c4e3103d55068287f5b55ae1ba3417aaba3e68d0
Pariharx7 <parihar.peace@gmail.com>
2026-08-27 11:27:37 +0530

    added cn utility support

--- client/src/layouts/NavLinks.jsx
+++ client/src/layouts/NavLinks.jsx
@@ -1,8 +1,9 @@
 import { NavLink } from 'react-router-dom';
+import { cn } from "@config/utils.js";
 
 const NavLinks = ({ label, Icon, link, onClick }) => {
     const active =
-        'flex w-full items-center space-x-3 rounded-md border-transparent bg-primary/5 dark:bg-primary-400/20 px-4 py-2.5 text-sm font-medium text-primary-500 dark:text-white ring-transparent transition-colors duration-100';
+        'flex w-full items-center space-x-3 rounded-md border-transparent bg-primary/5 dark:bg-primary-400/20 px-4 py-2.5 text-sm font-medium text-[var(--primary)] dark:text-white ring-transparent transition-colors duration-100';
     const inactive =
         'flex w-full items-center space-x-3 rounded-md border-transparent px-4 py-2.5 text-sm dark:text-gray-300 ring-transparent transition-colors duration-100 hover:bg-gray-50 dark:hover:bg-dark-800/40 dark:hover:text-black';
 
@@ -14,7 +15,10 @@ const NavLinks = ({ label, Icon, link, onClick }) => {
         >
             <
                 Icon
-                className="h-5 flex-none transition-colors dark:filter"
+                className={cn(
+                    "h-5 flex-none transition-colors dark:filter",
+                    ({ isActive }) => (isActive ? active : inactive)
+                )}
             />
             {label && <span className="truncate hover:text-clip">{label}</span>}
         </NavLink>
