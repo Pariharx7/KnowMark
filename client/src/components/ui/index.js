596c9fc0a5338e48fd1176e2469be0c5247fefb7
Pariharx7 <parihar.peace@gmail.com>
2026-09-01 11:26:24 +0530

    updated index file for /components/ui/

--- client/src/components/ui/index.js
+++ client/src/components/ui/index.js
@@ -2,5 +2,22 @@ import InputBox from "./InputBox";
 import PasswordBox from "./PasswordBox";
 import Button from "./Button";
 import SearchBox from "./SearchBox";
+import ErrorSnackBar from "./ErrorSnackBar";
+import FormSnackBar from "./FormSnackBar";
+import TagInput from "./TagInput";
+import SearchResults from "./SearchResults";
+import TextEditor from "./TextEditor";
+import FeatureCard from "./FeatureCard";
 
-export { InputBox, PasswordBox, Button, SearchBox };
+export {
+  InputBox,
+  PasswordBox,
+  Button,
+  SearchBox,
+  SearchResults,
+  FormSnackBar,
+  ErrorSnackBar,
+  TagInput,
+  TextEditor,
+  FeatureCard,
+};
