b85eaf9b8a5a37feef437a76d8f79d52d8444216
Pariharx7 <parihar.peace@gmail.com>
2026-09-02 11:06:12 +0530

    updated index file for /common/controllers/

--- server/src/api/common/controllers/index.js
+++ server/src/api/common/controllers/index.js
@@ -1,3 +1,8 @@
 import { serverHealthCheck } from "./healthcheck.controller.js";
+import {
+  addFeatures,
+  getFeatures,
+  editFeatures,
+} from "./features.controller.js";
 
-export { serverHealthCheck };
+export { serverHealthCheck, addFeatures, getFeatures, editFeatures };
