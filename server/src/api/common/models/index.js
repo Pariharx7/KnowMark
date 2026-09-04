d35cdceb296a082590044a6cac4276bc95196b02
Pariharx7 <parihar.peace@gmail.com>
2026-09-04 10:57:07 +0530

    updated index file for /common/models/

--- server/src/api/common/models/index.js
+++ server/src/api/common/models/index.js
@@ -1,4 +1,5 @@
 import { User } from "./user.model.js";
 import { Bookmark } from "./bookmark.model.js";
+import { Features } from "./features.model.js";
 
-export { User, Bookmark };
+export { User, Bookmark, Features };
