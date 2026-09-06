6dafdae2f7eb30d826f5f13cd6a7ea35cce6a903
Pariharx7 <parihar.peace@gmail.com>
2026-09-06 11:10:40 +0530

    added featureIcons config file

---
+++ client/src/config/featureIcons.js
@@ -0,0 +1,15 @@
+import {
+  FaRegFileCode,
+  FaConnectdevelop,
+  FaRegHeart,
+  FaNodeJs,
+  FaSlidersH,
+} from "react-icons/fa";
+
+export const featureIcons = {
+  0: FaRegFileCode,
+  1: FaConnectdevelop,
+  2: FaRegHeart,
+  3: FaNodeJs,
+  4: FaSlidersH,
+};
