5e2168384edbb836e19652a956d241da7e3d70c5
Pariharx7 <parihar.peace@gmail.com>
2026-09-08 11:00:40 +0530

    theme style changes

--- client/src/layouts/NavLinks.jsx
+++ client/src/layouts/NavLinks.jsx
@@ -3,9 +3,9 @@ import { cn } from "@config/utils.js";
 
 const NavLinks = ({ label, Icon, link, onClick }) => {
     const active =
-        'flex w-full items-center space-x-3 rounded-md border-transparent bg-primary/5 dark:bg-primary-400/20 px-4 py-2.5 text-sm font-medium text-[var(--primary)] dark:text-white ring-transparent transition-colors duration-100';
+        'flex w-full items-center space-x-3 rounded-md border-transparent bg-primary/5 dark:bg-primary-400/20 px-4 py-2.5 text-sm font-medium text-[var(--primary)] dark:text-white ring-transparent transition-colors duration-100 active:justify-center';
     const inactive =
-        'flex w-full items-center space-x-3 rounded-md border-transparent px-4 py-2.5 text-sm dark:text-gray-300 ring-transparent transition-colors duration-100 hover:bg-gray-50 dark:hover:bg-dark-800/40 dark:hover:text-black';
+        'flex w-full items-center space-x-3 rounded-md border-transparent px-4 py-2.5 text-sm ring-transparent transition-colors duration-100 hover:bg-gray-400 dark:hover:bg-gray-800/70 hover:justify-center';
 
     return (
         <NavLink
@@ -20,7 +20,7 @@ const NavLinks = ({ label, Icon, link, onClick }) => {
                     ({ isActive }) => (isActive ? active : inactive)
                 )}
             />
-            {label && <span className="truncate hover:text-clip">{label}</span>}
+            {label && <span className="truncate">{label}</span>}
         </NavLink>
     );
 };
