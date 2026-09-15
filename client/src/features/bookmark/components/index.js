a306f3926febfb043783a00c8d17d5175e91d395
Pariharx7 <parihar.peace@gmail.com>
2026-09-15 10:48:47 +0530

    index file for /bookmark/components/

---
+++ client/src/features/bookmark/components/index.js
@@ -0,0 +1,15 @@
+import BookmarkItem from "./BookmarkItem";
+import StarredBookmarks from "./StarredBookmarks";
+import SearchBookmarks from "./SearchBookmarks";
+import RecentBookmarks from "./RecentBookmarks";
+import BookmarkForm from "./BookmarkForm";
+import DummyBookmark from "./DummyBookmark";
+
+export {
+  BookmarkForm,
+  SearchBookmarks,
+  BookmarkItem,
+  StarredBookmarks,
+  RecentBookmarks,
+  DummyBookmark,
+};
