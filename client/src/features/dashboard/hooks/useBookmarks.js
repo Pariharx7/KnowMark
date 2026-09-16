321f10acb2accbd451df4a429c35f6f436184f4a
Pariharx7 <parihar.peace@gmail.com>
2026-09-16 11:04:33 +0530

    updated useBookmarks hook

--- client/src/features/dashboard/hooks/useBookmarks.js
+++ client/src/features/dashboard/hooks/useBookmarks.js
@@ -8,9 +8,7 @@ const useBookmarks = (keyName, serviceName, page, limit = 6) => {
   return useSuspenseQuery({
     queryKey: [`${keyName}`],
     queryFn: async () => {
-      // await new Promise((resolve) => setTimeout(resolve, 3000));
       const response = await serviceName;
-      console.log("ress  ", response);
       return response.data.data;
     },
     placeholderData: keepPreviousData,
