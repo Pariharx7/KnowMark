d8eb563ca9d928e07aaf9efc8bf535b8d209618c
Pariharx7 <parihar.peace@gmail.com>
2026-09-18 11:57:56 +0530

    updated UserDetails component

--- client/src/features/users/components/UserDetails.jsx
+++ client/src/features/users/components/UserDetails.jsx
@@ -16,8 +16,8 @@ const UserDetails = () => {
     return (
         <section className="w-full lg:max-w-screen p-1 lg:p-3 grid gap-2 md:grid-cols-2 flex-1">
             <div className="col-span-1 py-12 px-3 text-2xl w-full lg:max-w-2xl mx-auto flex flex-col rounded-xl justify-center items-center gap-3 border bg-card h-full">
-                <div className='bg-main fg-main h-96 w-full rounded-2xl my-10 px-10 text-9xl text-yellow-100 mx-auto'>
-                    <HiUserCircle className="size-99 mx-auto" />
+                <div className='bg-main fg-main h-40 lg:h-96 w-full rounded-2xl my-10 px-10 text-9xl text-yellow-100 mx-auto'>
+                    <HiUserCircle className="size-49 lg:size-99 mx-auto" />
                 </div>
 
                 <div className="-mt-9">Hey 👋 <span className="text-3xl">{currentUser?.name}</span></div>
@@ -38,7 +38,7 @@ const UserDetails = () => {
             <div className="col-span-1 w-full bg-main fg-card h-full max-w-2xl rounded-xl mx-auto px-2 py-3 flex flex-col gap-2 text-slate-700">
 
                 <div className='bg-card border h-64 md:h-full w-full rounded-2xl p-2 text-2xl py-4 flex flex-col items-center justify-evenly'>
-                    <p className="text-3xl font-semibold underline">Bookmarks Information: </p>
+                    <p className="text-3xl font-semibold underline text-center">Bookmarks Information: </p>
                     <div>
                         <p>Bookmarks: <Link to="/dashboard">{currentUser?.totalBookmarks}</Link></p>
                         <p>StarredBookmarks: <Link to="/starred">{currentUser?.starredBookmarks?.length}</Link></p>
@@ -47,7 +47,7 @@ const UserDetails = () => {
                 </div>
                 <div className='bg-card border h-64 md:h-full w-full rounded-2xl p-2 text-2xl py-4 flex flex-col items-center justify-evenly'>
                     <p className="text-3xl font-semibold underline">Account Information: </p>
-                    <div>
+                    <div className='tracking-tight lg:tracking-normal'>
                         <p>Sign up method: <span className="ml-2">{currentUser?.signInType}</span></p>
                         <p>Joined on: <span className="ml-5">{currentUser?.createdAt?.slice(0, 7)}</span></p>
                         <p>Last Updated at: <span className="ml-5">{currentUser?.updatedAt?.slice(0, 10)}</span></p>
