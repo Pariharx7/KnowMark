f427113eb5c806b3aac7e025d36b642831eb3099
Pariharx7 <parihar.peace@gmail.com>
2026-09-19 10:56:03 +0530

    added Bookmark page

--- client/src/pages/Bookmark.jsx
+++ client/src/pages/Bookmark.jsx
@@ -3,20 +3,20 @@ import { BookmarkItem } from "@features/bookmark";
 import { useBookmarks } from "@features/dashboard";
 import { BookmarkService } from "@features/bookmark";
 import { BOOKMARK_QUERY_KEYS } from "@config/constants";
-import { useCallback, useEffect, useState } from "react";
+import { Suspense, useCallback, useEffect, useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const Bookmark = () => {
 
     const { id } = useParams();
     const navigate = useNavigate();
+    const keyName = `'${BOOKMARK_QUERY_KEYS.getBookmark}', ${id}`
 
-    //need to check isPending
     const { data: {
         bookmark,
         isStarred
-    }, isPending, refetch } = useBookmarks(
-        BOOKMARK_QUERY_KEYS.getBookmark,
+    } } = useBookmarks(
+        keyName,
         BookmarkService.getBookmarkById(id)
     );
 
@@ -24,21 +24,18 @@ const Bookmark = () => {
     const [loadingId, setLoadingId] = useState(id);
     const [message, setMessage] = useState("");
 
-    // console.log("starred 16 outside   ", bookmark.id)
 
     const handleClick = useCallback(async (id) => {
         setLoadingId(id);
 
         setStarred(prev => !prev);
 
-        console.log("starred 37 inside try  ", starred)
         const service = starred
             ? BookmarkService.unstarBookmark(id)
             : BookmarkService.starBookmark(id);
 
         try {
             const response = await service;
-            // console.log("star response: ", response);
             if (response.data.success) {
                 setMessage(response.data.message);
             }
@@ -69,17 +66,18 @@ const Bookmark = () => {
 
     return (
         <div className="bg-main fg-main">
-            {isPending ? <div className="flex text-3xl text-black">Loading... </div> :
-                <div>
-                    <BookmarkItem
-                        data={bookmark}
-                        onStar={() => handleClick(id)}
-                        onDelete={() => deleteMutation.mutate(id)}
-                        onEdit={() => handleEdit(id, bookmark)}
-                        disabled={deleteMutation.isPending}
-                    />
-                </div>
-            }
+            {/* <Suspense fallback={<div className="flex text-3xl text-black">Loading... </div>}> */}
+            <div>
+                <BookmarkItem
+                    data={bookmark}
+                    onStar={() => handleClick(id)}
+                    onDelete={() => deleteMutation.mutate(id)}
+                    onEdit={() => handleEdit(id, bookmark)}
+                    disabled={deleteMutation.isPending}
+                />
+            </div>
+            {/* </Suspense> */}
+
         </div>
     )
 }
