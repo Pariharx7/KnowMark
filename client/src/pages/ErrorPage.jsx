2ce5a517225b92fe2ef73752934d0f2b6edeb779
Pariharx7 <parihar.peace@gmail.com>
2026-09-20 10:56:19 +0530

    Added the Error page

---
+++ client/src/pages/ErrorPage.jsx
@@ -0,0 +1,8 @@
+const ErrorPage = () => {
+    return <div className="flex flex-col items-center justify-center text-shadow-teal-500 text-orange-400 shadow-2xl h-full">
+        <div className="text-9xl">404</div>
+        <p className="text-3xl">Page Not Found</p>
+    </div>
+}
+
+export default ErrorPage;
