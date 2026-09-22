60e5a08dc30f7ef77461e897bd09a3dfae4144cd
Pariharx7 <parihar.peace@gmail.com>
2026-09-22 11:48:21 +0530

    index file for /pages/

---
+++ client/src/pages/index.js
@@ -0,0 +1,27 @@
+import Signup from "./Signup";
+import Signin from "./Signin";
+import Dashboard from "./Dashboard";
+import Profile from "./Profile";
+import ProfileSettings from "./ProfileSettings";
+import CreateBookmark from "./CreateBookmark"; // rename to bookmark or something short
+import Starred from "./Starred";
+import Bookmark from "./Bookmark";
+import SearchPage from "./SearchPage";
+import EditBookmark from "./EditBookmark";
+import LandingPage from "./LandingPage";
+import ErrorPage from "./ErrorPage";
+
+export {
+  Signup,
+  Signin,
+  Dashboard,
+  Profile,
+  ProfileSettings,
+  CreateBookmark,
+  Starred,
+  Bookmark,
+  SearchPage,
+  EditBookmark,
+  LandingPage,
+  ErrorPage,
+};
